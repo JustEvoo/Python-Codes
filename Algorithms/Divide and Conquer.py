71b2b07f08d2d6bac794ab6b05dbcb6dbbf3b3f7
Justinevorius <139213355+JustEvoo@users.noreply.github.com>
2026-09-15 21:50:08 +0700

    Add sample numbers and clean up formatting
    Added a sample list of numbers and improved formatting.

--- Algorithms/Divide and Conquer.py
+++ Algorithms/Divide and Conquer.py
@@ -1,19 +1,20 @@
 import time
 
+num = [1,5,2,7,3,6,5,8,6,4]
+
+#Divides the numbers into smaller pieces
 def test(arr):
 	if len(arr) > 1:
 		left_side = arr[:len(arr)//2]
 		right_side = arr[len(arr)//2:]
-
 			
 		left_sort = test(left_side)
 		right_sort = test(right_side)
-			
 		return merge(left_sort, right_sort)
 	else:
-		return arr
-			
-	
+		return arr	
+		
+#Rearranges numbers into order from comparing the left side and right side
 def merge(left, right):
 	result = []
 	i = j = 0
@@ -24,12 +25,12 @@ def merge(left, right):
 		else:
 			result.append(right[j])
 			j += 1
-			
+	#Cleanup for leftover numbers in list
 	result.extend(left[i:])
 	result.extend(right[j:])
 	return result
-num = [1,5,2,7,3,6,5,8,6,4]
-
+	
+#Output
 print(f"Your numbers: {num}")
 for i in range(15):
 	dots = "."
