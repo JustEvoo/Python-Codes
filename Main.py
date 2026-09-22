a69f48fad935549628ca320bc4c2cb0bf483515c
JustinEvorius <justinevorius2009@gmail.com>
2026-09-22 21:32:26 +0700

    Initial Upload

--- Main.py
+++ Main.py
@@ -1,11 +1,31 @@
-def add(*nums):
-    total = 0
-    for arg in nums:
-        total += arg
-    return total
+def showbalance(Bal):
+    print(f"Your balance is ${Bal:.2f}")
 
-def address(**kwargs):
-    for key, value in kwargs.items():
-        print(f"{key}: {value}")
+def deposit():
+    pass
 
-address(Country="Indonesia", city="Yogyakarta")
+def withdraw():
+    pass
+
+balance = 0
+
+IsRunning = True
+
+while IsRunning:
+    print("Banking Program")
+    print("1.Show Balance")
+    print("2.Deposit")
+    print("3.Withdraw")
+    print("4.Exit")
+
+    choice = input("Please enter your choice (1-4): ")
+    if choice == "1":
+        showbalance()
+    elif choice == "2":
+        deposit()
+    elif choice == "3":
+        withdraw()
+    elif choice == "4":
+        exit()
+    else:
+        print("That isn't a valid choice")
